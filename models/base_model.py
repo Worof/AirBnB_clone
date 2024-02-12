ab13b9631e43980f3a49adf09e7eb4bb96acab2b
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-02-12 23:08:47 +0200

    BaseMode

--- models/base_model.py
+++ models/base_model.py
@@ -5,27 +5,28 @@ class BaseModel:
     """Defines all common attributes/methods for other classes."""
     
     def __init__(self, *args, **kwargs):
-        """Initializes a new instance of BaseModel."""
-        self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = datetime.now()
+        """Initialize a new instance of BaseModel."""
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.fromisoformat(value)
                 if key != "__class__":
                     setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
     
     def __str__(self):
-        """Returns a string representation of the BaseModel instance."""
+        """Return a string representation of the BaseModel instance."""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
     
     def save(self):
-        """Updates the updated_at attribute with the current datetime."""
+        """Update the public instance attribute `updated_at` with the current datetime."""
         self.updated_at = datetime.now()
     
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of the instance."""
-        dict_repr = self.__dict__.copy()
+        """Return a dictionary containing all keys/values of the instance."""
+        dict_repr = dict((key, value) for key, value in self.__dict__.items() if not key.startswith('__'))
         dict_repr["__class__"] = self.__class__.__name__
         dict_repr["created_at"] = self.created_at.isoformat()
         dict_repr["updated_at"] = self.updated_at.isoformat()
