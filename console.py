309eb7242682e850b71c71680ea3401eb17a22f0
Worof <worof.eng_5760@eng.kfs.edu.eg>
2024-02-13 01:29:43 +0200

    More classes!

--- console.py
+++ console.py
@@ -1,130 +1,99 @@
 #!/usr/bin/python3
 import cmd
 from models.base_model import BaseModel
-from models.user import User
 from models.engine.file_storage import FileStorage
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
-class HBNBCommand(cmd.Cmd):
-    """Command interpreter"""
-    prompt = "(hbnb) "
-    classes = {"BaseModel": BaseModel, "User": User}
-
-    def do_create(self, line):
-        """Create a new instance of BaseModel, save it and print the id"""
-        args = line.split()
-        if len(args) == 0:
+class HBNBCommand:
+    """Entry point of the command interpreter"""
+    
+    def do_create(self, arg):
+        """Create a new instance of BaseModel"""
+        if not arg:
             print("** class name missing **")
             return
-        class_name = args[0]
-        if class_name not in self.classes:
+        try:
+            new_instance = eval(arg)()
+            new_instance.save()
+            print(new_instance.id)
+        except NameError:
             print("** class doesn't exist **")
-            return
-        new_instance = self.classes[class_name]()
-        new_instance.save()
-        print(new_instance.id)
 
-    def do_show(self, line):
-        """Print the string representation of an instance"""
-        args = line.split()
-        if len(args) == 0:
+    def do_show(self, arg):
+        """Prints the string representation of an instance"""
+        if not arg:
             print("** class name missing **")
             return
-        class_name = args[0]
-        if class_name not in self.classes:
-            print("** class doesn't exist **")
-            return
+        args = arg.split()
         if len(args) < 2:
             print("** instance id missing **")
             return
+        class_name = args[0]
         instance_id = args[1]
-        storage = FileStorage()
-        storage.reload()
-        all_objs = storage.all()
-        key = "{}.{}".format(class_name, instance_id)
-        if key not in all_objs:
+        try:
+            objects = FileStorage().all()
+            key = "{}.{}".format(class_name, instance_id)
+            print(objects[key])
+        except KeyError:
             print("** no instance found **")
-        else:
-            print(all_objs[key])
 
-    def do_destroy(self, line):
-        """Delete an instance based on the class name and id"""
-        args = line.split()
-        if len(args) == 0:
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        if not arg:
             print("** class name missing **")
             return
-        class_name = args[0]
-        if class_name not in self.classes:
-            print("** class doesn't exist **")
-            return
+        args = arg.split()
         if len(args) < 2:
             print("** instance id missing **")
             return
+        class_name = args[0]
         instance_id = args[1]
-        storage = FileStorage()
-        storage.reload()
-        all_objs = storage.all()
-        key = "{}.{}".format(class_name, instance_id)
-        if key not in all_objs:
+        try:
+            objects = FileStorage().all()
+            key = "{}.{}".format(class_name, instance_id)
+            del objects[key]
+            FileStorage().save()
+        except KeyError:
             print("** no instance found **")
-        else:
-            del all_objs[key]
-            storage.save()
 
-    def do_all(self, line):
+    def do_all(self, arg):
         """Prints all string representation of all instances"""
-        args = line.split()
-        storage = FileStorage()
-        storage.reload()
-        all_objs = storage.all()
-        if len(args) == 0:
-            print([str(obj) for obj in all_objs.values()])
-        else:
-            class_name = args[0]
-            if class_name not in self.classes:
-                print("** class doesn't exist **")
-                return
-            print([str(obj) for key, obj in all_objs.items() if key.startswith(class_name)])
-
-    def do_update(self, line):
-        """Updates an instance based on the class name and id"""
-        args = line.split()
-        if len(args) == 0:
-            print("** class name missing **")
+        objects = FileStorage().all()
+        if not arg:
+            print([str(obj) for obj in objects.values()])
             return
-        class_name = args[0]
-        if class_name not in self.classes:
+        args = arg.split()
+        if args[0] not in ["BaseModel", "User", "State", "City", "Amenity", "Place", "Review"]:
             print("** class doesn't exist **")
             return
+        print([str(obj) for obj in objects.values() if args[0] == obj.__class__.__name__])
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id"""
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split()
         if len(args) < 2:
             print("** instance id missing **")
             return
+        class_name = args[0]
         instance_id = args[1]
-        storage = FileStorage()
-        storage.reload()
-        all_objs = storage.all()
-        key = "{}.{}".format(class_name, instance_id)
-        if key not in all_objs:
-            print("** no instance found **")
-            return
         if len(args) < 3:
             print("** attribute name missing **")
             return
         if len(args) < 4:
             print("** value missing **")
             return
-        attribute_name = args[2]
-        attribute_value = args[3]
-        setattr(all_objs[key], attribute_name, attribute_value)
-        all_objs[key].save()
-
-    def do_quit(self, line):
-        """Quit command to exit the program"""
-        return True
-
-    def do_EOF(self, line):
-        """EOF command to exit the program"""
-        print("")
-        return True
-
-if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+        try:
+            objects = FileStorage().all()
+            key = "{}.{}".format(class_name, instance_id)
+            obj = objects[key]
+            setattr(obj, args[2], args[3])
+            obj.save()
+        except KeyError:
+            print("** no instance found **")
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,33 +4,35 @@ import json
 from models.base_model import BaseModel
 
 class FileStorage:
-    """Serializes instances to a JSON file and deserializes JSON file to instances."""
-    __file_path = 'file.json'
+    """Serializes instances to a JSON file and deserializes JSON file to instances"""
+
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects."""
+        """Returns the dictionary __objects"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id."""
-        key = obj.__class__.__name__ + '.' + obj.id
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)."""
-        obj_dict = {obj: self.__objects[obj].to_dict() for obj in self.__objects.keys()}
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(obj_dict, f)
+        """Serializes __objects to the JSON file"""
+        new_dict = {}
+        for key, value in FileStorage.__objects.items():
+            new_dict[key] = value.to_dict()
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
+            json.dump(new_dict, file)
 
     def reload(self):
-        """Deserializes the JSON file to __objects if file exists, otherwise do nothing."""
+        """Deserializes the JSON file to __objects"""
         try:
-            with open(FileStorage.__file_path, 'r') as f:
-                objs = json.load(f)
-                for obj in objs.values():
-                    cls_name = obj['__class__']
-                    del obj['__class__']
-                    self.new(eval(cls_name)(**obj))
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    self.new(eval(class_name)(**value))
         except FileNotFoundError:
             pass
